cd9eac5feec1f0f278051d2cc8ac172c6a44ce6f
Ding Yanwen <CYS2409009@xmu.edu.my>
2025-10-06 16:20:23 +0800

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,9 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
 }
 
 int main(int argc, char* argv[]) {
-    // Flush after every std::cout / std::cerr
+    
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here" << std::endl;
 
     if (argc != 3) {
         std::cerr << "Expected two arguments" << std::endl;
@@ -31,19 +28,17 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Uncomment this block to pass the first stage
-    //
-    // std::string input_line;
-    // std::getline(std::cin, input_line);
-    //
-    // try {
-    //     if (match_pattern(input_line, pattern)) {
-    //         return 0;
-    //     } else {
-    //         return 1;
-    //     }
-    // } catch (const std::runtime_error& e) {
-    //     std::cerr << e.what() << std::endl;
-    //     return 1;
-    // }
+    std::string input_line;
+    std::getline(std::cin,input_line);
+
+    try{
+        if(match_pattern(input_line,pattern)){
+            return 0;
+        }else{
+            return 1;
+        }
+    }catch (const std::runtime_error& e){
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 }
